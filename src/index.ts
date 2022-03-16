022d2f87f18d4d7d6e67637b706b885983f4d9ad
daniel-rif <danielonyenwee@gmail.com>
2022-03-16 19:47:38 +0100

    set view engine, view files directory and static files directory

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
+import bodyParser from "body-parser"
 import express from "express"
+import { join } from "path"
 
 const PORT = process.env.PORT || 3400
 
 const app = express()
+
+app.set("view engine", "ejs")
+
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.set('trust proxy', 1)
+
+app.set("views", join(__dirname, "../views"))
+
+app.use(express.static(join(__dirname, "../public")))
 
 app.get("/", (req, res) => res.send({ message: "Welcome to pico" }));
 
